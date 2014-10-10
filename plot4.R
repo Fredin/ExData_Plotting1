796a52be032021e745fa6726e3bd5ef31a802f2e
Fredin <esteban.fredin@gmail.com>
2014-10-10 03:33:07 -0500

    Fix Plot4
    The last plot in plot4 displayed the wrong variable. Now it's fixed.

--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ lines(newtime, data$Sub_metering_3, col="blue")
 legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
        col = c("black", "red", "blue"), lty = c(1,1,1), lwd = c(1,1,1), bty = "n")
 
-plot(newtime, data$Global_active_power, type = "l", xlab = "datetime", 
+plot(newtime, data$Global_reactive_power, type = "l", xlab = "datetime", 
      ylab = "Global reactive Power (kilowatts)")
 
 dev.off()
